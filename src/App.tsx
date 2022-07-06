f4cfe3d2a39ce7d23be15f6a6d2dd4f4a88507b0
Roman Cabalum <roman.cabalum@gmail.com>
2022-07-06 21:00:29 +0800

    finished intial jsx structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useContext } from "react";
+import "./App.css";
+import LoginScreen from "./components/LoginScreen";
+import LoginContext from "./Context";
 
 function App() {
-  return (
-    <div className="bg-red-50">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const userContext = useContext(LoginContext);
+  return <>{!userContext.isLoggedIn && <LoginScreen />}</>;
 }
 
 export default App;
---
+++ src/components/LoginScreen.tsx
@@ -0,0 +1,62 @@
+import logo from "../logo512.png";
+
+const LoginScreen = () => {
+  return (
+    <div className="flex-col gap-y-4 flex items-center justify-center h-screen w-screen">
+      <div className="flex items-center justify-center">
+        <img src={logo} className="w-20"></img>
+      </div>
+      <div className="flex flex-col gap-y-2 items-center justify-center">
+        <h1 className="text-4xl font-bold"> Sign in to Slack</h1>
+        <p>
+          We suggest using the
+          <span className="font-bold"> email you use at work.</span>
+        </p>
+      </div>
+      <div className="flex flex-col gap-y-2">
+        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          Sign in with Google
+        </button>
+        <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+          Sign in with Apple
+        </button>
+      </div>
+      <div className="flex flex-row gap-x-4 justify-center items-center w-screen">
+        <hr className="w-56"></hr>
+        <span>OR</span>
+        <hr className="w-56"></hr>
+      </div>
+      <form
+        method="post"
+        action="/"
+        onClick={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <div className="flex flex-col gap-y-2 justify-center">
+          <div>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="name@work-email.com"
+            ></input>
+          </div>
+          <div>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="Password"
+            ></input>
+          </div>
+          <button className="bg-fuchsia-900 hover:bg-fuchsia-700 text-white font-bold py-2 px-4 rounded">
+            Sign in with email
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LoginScreen;
