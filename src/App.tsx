bf90adc02a0111d010c4b45000d0d07b270c3d1b
Jose Robelle <robellempajarin@gmail.com>
2022-07-07 18:38:00 +0800

    added sidebar and header components UI and the firebase config

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import SideBar from "./components/Sidebar";
 
 function App() {
   return (
-    <div className="bg-red-50">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid grid-rows-[40px,1fr] grid-cols-[260px,1fr] h-screen w-full ">
+      <Header />
+      <SideBar />
+
+      {/* <div> Chat goes here</div> */}
     </div>
   );
 }
